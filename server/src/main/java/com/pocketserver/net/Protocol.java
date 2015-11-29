25d92726cff31c16d83a09265e451b1051d0c52e
Connor Spencer Harries <connor@smirking.ninja>
2015-11-29 12:25:51 +0000

    Allow section character to be transmitted as a string.

--- server/src/main/java/com/pocketserver/net/Protocol.java
+++ server/src/main/java/com/pocketserver/net/Protocol.java
@@ -4,7 +4,7 @@ import java.security.SecureRandom;
 import java.util.regex.Pattern;
 
 public interface Protocol {
-    Pattern DISALLOWED_CHARS = Pattern.compile("[^" + Pattern.quote(" !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~") + "]");
+    Pattern DISALLOWED_CHARS = Pattern.compile("[^" + Pattern.quote(" \u00A7!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~") + "]");
     String TEMP_IDENTIFIER = "MCPE;Test server;35;0.13.0; 0;20;20";
     long SERVER_ID = new SecureRandom().nextLong();
     long MAGIC_1 = 0x00ffff00fefefefeL;
