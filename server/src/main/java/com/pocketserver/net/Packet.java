60c789ec9abb329e67ffb975579480741d218e48
Connor Spencer Harries <connor@smirking.ninja>
2015-11-29 11:59:36 +0000

    Add Packet#writeAddress(ByteBuf, InetSocketAddress) and Packet#readAddress(ByteBuf) convenience methods.

--- server/src/main/java/com/pocketserver/net/Packet.java
+++ server/src/main/java/com/pocketserver/net/Packet.java
@@ -3,7 +3,12 @@ package com.pocketserver.net;
 import com.google.common.base.Charsets;
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Preconditions;
+import com.google.common.net.InetAddresses;
 
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 import java.util.List;
 
 import io.netty.buffer.ByteBuf;
@@ -49,5 +54,45 @@ public abstract class Packet {
         byte[] data = str.getBytes(Charsets.UTF_8);
         buf.writeShort(data.length);
         buf.writeBytes(data);
+    }
+
+    protected final void writeAddress(ByteBuf buf, InetSocketAddress address) {
+        InetAddress addr;
+        if (address.isUnresolved()) {
+            try {
+                addr = InetAddress.getByName(address.getHostName());
+            } catch (UnknownHostException e) {
+                return;
+            }
+        } else {
+            addr = address.getAddress();
+        }
+
+        if (addr.getClass() == Inet6Address.class) {
+            Inet6Address temp = (Inet6Address) addr;
+            try {
+                addr = InetAddresses.getEmbeddedIPv4ClientAddress(temp);
+            } catch (IllegalArgumentException ex) {
+                throw new IllegalArgumentException("address type not supported", ex);
+            }
+        }
+
+        buf.writeByte(0x04);
+        for (byte b : addr.getAddress()) {
+            buf.writeByte(~b & 0xFF);
+        }
+        buf.writeShort(address.getPort());
+    }
+
+    protected final InetSocketAddress readAddress(ByteBuf buf) {
+        Preconditions.checkArgument(buf.readByte() == 0x04, "address type not supported");
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 4; i++) {
+            builder.append(~(buf.readByte() | ~0xFF));
+            builder.append(".");
+        }
+        builder.setLength(builder.length() - 1);
+        short port = buf.readShort();
+        return new InetSocketAddress(builder.toString(), port);
     }
 }
--- server/src/main/java/com/pocketserver/net/packet/connect/PacketConnectOpenResponse.java
+++ server/src/main/java/com/pocketserver/net/packet/connect/PacketConnectOpenResponse.java
@@ -1,9 +1,14 @@
 package com.pocketserver.net.packet.connect;
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
 import com.pocketserver.net.packet.AbstractEncapsulatedPacket;
 import io.netty.buffer.ByteBuf;
 
 public class PacketConnectOpenResponse extends AbstractEncapsulatedPacket {
+    private static final InetSocketAddress SYSTEM_ADDRESS = new InetSocketAddress(InetAddress.getLoopbackAddress(), 19132);
+
     private final long serverTimestamp;
     private final long clientTimestamp;
 
@@ -12,17 +17,9 @@ public class PacketConnectOpenResponse extends AbstractEncapsulatedPacket {
         this.clientTimestamp = clientTimestamp;
     }
 
-    // TODO: https://github.com/NiclasOlofsson/MiNET/blob/master/src/MiNET/MiNET/Net/MCPE%20Protocol%20Documentation.md#package-connection-request-accepted-0x10
     @Override
     public void write(ByteBuf buf) {
-        buf.writeByte(4);
-        {
-            buf.writeByte(~127);
-            buf.writeByte(~0);
-            buf.writeByte(~0);
-            buf.writeByte(~1);
-        }
-        buf.writeShort(19132);
+        writeAddress(buf, SYSTEM_ADDRESS);
         buf.writeLong(clientTimestamp);
         buf.writeLong(serverTimestamp);
     }
