186aadebcdc1390d05aa87f4144ce4f27c984b75
Connor Spencer Harries <connor@smirking.ninja>
2015-12-04 16:42:00 +0000

    Rework the custom packet system.

--- server/src/main/java/com/pocketserver/net/packet/raknet/PacketRaknetCustom.java
+++ server/src/main/java/com/pocketserver/net/packet/raknet/PacketRaknetCustom.java
@@ -1,9 +1,6 @@
 package com.pocketserver.net.packet.raknet;
 
-import com.google.common.collect.Lists;
-
 import java.util.List;
-import java.util.stream.IntStream;
 
 import com.pocketserver.api.Server;
 import com.pocketserver.api.util.PocketLogging;
@@ -14,27 +11,27 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 
 public class PacketRaknetCustom extends Packet {
-    private final List<EncapsulationStrategy> strategies;
     private ByteBuf content;
     private int count;
 
-    public PacketRaknetCustom() {
-        this.strategies = Lists.newArrayList();
-    }
-
     @Override
-    public void handle(ChannelHandlerContext ctx, List<Packet> out) {
+    public void handle(ChannelHandlerContext ctx, List<Packet> out) throws Exception {
         out.add(new PacketRaknetAck(count));
-        content.markReaderIndex();
         try {
-            for (EncapsulationStrategy strategy : strategies) {
-                strategy.decode(ctx, content, out);
+            for (int i = 0; i < count; i++) {
+                EncapsulationStrategy strategy = Encapsulation.fromId(content.readByte());
+                try {
+                    strategy.decode(ctx, content, out);
+                } catch (Exception cause) {
+                    Server.getServer().getLogger().error(PocketLogging.Server.NETWORK, "Failed to code packet {}/{}", new Object[]{
+                        String.valueOf(i),
+                        String.valueOf(count),
+                        cause
+                    });
+                    content.skipBytes(content.readableBytes());
+                    break;
+                }
             }
-        } catch (Exception ex) {
-            content.resetReaderIndex();
-            Server.getServer().getLogger().error(PocketLogging.Server.NETWORK, "Failed to decode packet!", new Object[]{
-                ex
-            });
         } finally {
             content.release();
         }
@@ -47,15 +44,5 @@ public class PacketRaknetCustom extends Packet {
         Server.getServer().getLogger().trace(PocketLogging.Server.NETWORK, "Packet contains {} encapsulated packets", new Object[]{
             String.valueOf(count)
         });
-        int originalIndex = content.readerIndex();
-        IntStream.range(0, count).forEachOrdered(value -> {
-            EncapsulationStrategy strategy = Encapsulation.fromId(content.readByte());
-            strategies.add(strategy);
-
-            int length = strategy.peekLength(content);
-            // TODO: Figure out if header has been sent to account for COUNT_UNKNOWN's mysterious 4 bytes
-            content.skipBytes(length);
-        });
-        content.readerIndex(originalIndex);
     }
 }
